1e669a6b5151f1d8ae35668a357c2bf3898cf2dd
pradip9696 <pradipsawant9696@gmail.com>
2025-11-07 12:35:07 +0530

    curd oprations create delete disp method using mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
+const usermodel=require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render("index");
 });
+
+
+
+// for creating the user
+
+router.get('/create',async function(req, res) {
+   const use1= await usermodel.create({
+       username:"pradipsawant",
+       name:"padip",
+       age:33
+    },{
+       username:"pritisawanrt",
+       name:"priti",
+       age:44
+    });
+
+
+    res.send(use1);
+  
+});
+
+// it create a usermode in databasse 
+
+// above all moethods releted to monogodb is asycronous so required to add the async await and create method always has a object inside it and the objects values we can write in any order
+
+
+
+
+// to get all users
+
+router.get("/getalluser", async function(req,res){
+ let all= await usermodel.find();
+ res.send(all);
+})
+
+// to get one user users
+
+router.get("/onefind", async function(req,res){
+ let all= await usermodel.findOne({name:"priti"});
+ res.send(all);
+})
+
+
+
+// now how to delete the user
+
+router.get("/delete",async function(req,res){
+ let ue = await usermodel.findOneAndDelete({
+    name:"padip"
+  });
+  res.send(ue);
+});
+
+
+
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+/* 1. install monogodb
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+2. install mongoosejs
+npm i mongoose
 
-module.exports = router;
+3. require and  setup connection 
+
+4. make Schema
+
+5. create model
+*/
+
+// 3. require and  setup connection 
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/practice")  
+// above line also create a database last name is db name 
+
+
+
+// 4. make Schema
+const userschema = mongoose.Schema({
+  username:String,
+  name:String,
+  age: Number
+})
+
+// 5. create model  1st partameter is name of the model(collection) we want to set aned another is schema and then export it.
+
+module.exports=mongoose.model("user",userschema);
+
+
